89590d71d424b49f453b40ef4045bda3ea6cdf72
Thomas Millward Wright <thomas.wright@mwrinfosecurity.com>
2015-06-08 10:12:58 +0100

    tidy up pipeableRows code

--- app/assets/javascripts/components/pipeables_table.js.jsx
+++ app/assets/javascripts/components/pipeables_table.js.jsx
@@ -1,11 +1,12 @@
 var PipeablesTable = React.createClass({
   render: function() {
-
-    var pipeableRows = this.props.data.map(function (pipeable) {
-      return (
-        <PipeableRow name={pipeable.name} state={pipeable.state}></PipeableRow>
-      )
-    });
+    var pipeableRows = this.props.data.map(
+      function (pipeable) {
+        return (
+          <PipeableRow name={pipeable.name} state={pipeable.state}/>
+        );
+      }
+    );
     return (
       <div>
         <h2>Pipeables</h2>
